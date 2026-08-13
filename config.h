4394bb577229ddd5bef98f17a00a3039df2ba3e0
pomarrr <hubertui1234@gmail.com>
2026-08-14 00:22:45 +0200

    +bar-height +winicom

--- config.h
+++ config.h
@@ -10,6 +10,9 @@ static const unsigned int gappov    = 5;       /* vert outer gap between windows
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+#define ICONSIZE 16   /* icon size */
+#define ICONSPACING 5 /* space between icon and title */
+static const int user_bh            = 6;        /* 2 is the default spacing around the bar's font */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
