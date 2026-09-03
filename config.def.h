d7e30ca95c5f11f67dd74a2d4728eaaab0138604
pomar <115939501+pomarrr@users.noreply.github.com>
2026-09-03 16:37:11 +0200

    changed layout symbols

--- config.def.h
+++ config.def.h
@@ -55,8 +55,8 @@ static const int refreshrate = 120;  /* refresh rate (per second) for client mov
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "Tiling",      tile },    /* first entry is default */
-	{ "Floating",    NULL },    /* no layout function means floating behavior */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",    NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
